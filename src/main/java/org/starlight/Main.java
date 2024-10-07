7b50f41dfaed66379956f399705be04ac5fbb7d7
Guilherme Santos <guilherme460@yahoo.com.br>
2024-10-07 18:13:44 -0300

    Start

--- src/main/java/org/starlight/Main.java
+++ src/main/java/org/starlight/Main.java
@@ -2,7 +2,7 @@ package org.starlight;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Word");
+        System.out.println("Start");
 
     }
 }
